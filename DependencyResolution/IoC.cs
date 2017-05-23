a08497d80efae31fe799fe4be98e970b1d36c0d4
Sandy <sandyz@bizcover.com.au>
2017-05-24 09:22:25 +1000

    ioc

---
+++ DependencyResolution/IoC.cs
@@ -0,0 +1,14 @@
+﻿using BizCover.Address.Search.Business;
+using BizCover.Address.Search.BusinessInterfaces;
+using Ninject;
+
+namespace BizCover.Address.Search.DependencyResolution
+{
+    public class IoC
+    {
+        public static void ConfigureIoC(IKernel kernel)
+        {
+            kernel.Bind<IBCAddressSearchService>().To<BCAddressSearchService>().InSingletonScope();
+        }
+    }
+}
